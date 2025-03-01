f1a0564b97db86ab98cb7031fb4f68cd715398b2
Ade <ade@Elizabeths-MacBook-Pro.local>
2025-03-01 21:26:43 +0100

    week 8 exercise

--- lib/website-stack.ts
+++ lib/website-stack.ts
@@ -1,16 +1,66 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'
 
 export class WebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, `${id}-VPC`, {
+      vpcName: `${id}-VPC`,
+      natGatewayProvider: ec2.NatGatewayProvider.gateway(),
+      natGateways: 2,
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      gatewayEndpoints: {
+        S3: {
+          service: ec2.GatewayVpcEndpointAwsService.S3
+        }
+      },
+      createInternetGateway: true,
+      availabilityZones: [
+        'eu-central-1a',
+        'eu-central-1b'
+      ],
+      subnetConfiguration: [
+        {
+          cidrMask: 20,
+          name: "public-subnet",
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 20,
+          name: "private-subnet",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+        },
+      ]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'WebsiteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new s3.Bucket(this, `${id}-Bucket`, {
+      bucketName: `acp-test-bucket-2025`
+    })
+
+    const instanceSG = new ec2.SecurityGroup(this, `${id}-InstanceSG`, {
+      vpc: vpc
+    })
+
+    const loadBalancerSG = new ec2.SecurityGroup(this, `${id}-LoadBalancerSG`, {
+      vpc: vpc
+    })
+
+    const instance = new ec2.Instance(this, `${id}-Instance`, {
+      instanceName: `${id}-Instance`,
+      vpc: vpc,
+      securityGroup: instanceSG,
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
+    })
+
+    const loadBalancer = new elbv2.ApplicationLoadBalancer(this, `${id}-LoadBalancer`, {
+      loadBalancerName: `${id}-LoadBalancer`,
+      securityGroup: loadBalancerSG,
+      vpc: vpc
+    })
   }
 }
